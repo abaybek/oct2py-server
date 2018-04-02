36a736efe5b13db79dd8faae63ceab19f451107b
Abay Bek <bekbaganbetov.abay@gmail.com>
2018-04-02 12:19:29 +0600

    authorization added

---
+++ front-end/octave-front/src/reducers/auth.js
@@ -0,0 +1,65 @@
+import jwtDecode from 'jwt-decode';
+import * as auth from '../actions/auth';
+
+const initialState = {
+  token: undefined,
+  error: {}
+}
+
+export function accessToken(state) {
+  if (state.access) {
+      return  state.token
+  }
+}
+
+export function refreshToken(state) {
+  if (state.refresh) {
+      return  state.token
+  }
+}
+
+export function isAuthenticated(state) {
+  if (state.token !== undefined){
+    return true
+  }else{
+    return false
+  }
+}
+
+export function errors(state) {
+  return  state.errors
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case auth.LOGIN_SUCCESS:
+      console.log('LOGIN_SUCCESS');
+      console.log(state);
+      console.log(action);
+      return {
+        token: {
+          token: action.payload.token,
+          ...jwtDecode(action.payload.token)
+        },
+        errors: {}
+      }
+    case auth.TOKEN_RECEIVED:
+      return {
+        ...state,
+        token: {
+          token: action.payload.token,
+          ...jwtDecode(action.payload.token)
+        }
+      }
+    case auth.LOGIN_FAILURE:
+    case auth.TOKEN_FAILURE:
+      return {
+        token: undefined,
+        errors: action.payload.response || {
+          'non_field_errors': action.payload.statusText
+        },
+      }
+    default:
+      return state
+  }
+}
